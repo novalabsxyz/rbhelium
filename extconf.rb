0120f7a8e0be631aec159d1328e742d49d2dd992
Andrew Thompson <andrew@hijacked.us>
2014-09-27 16:41:09 +0000

    Tweak linker flags and extend test.rb to also do a send

--- extconf.rb
+++ extconf.rb
@@ -13,5 +13,6 @@ must_have :library, 'crypto'
 must_have :library, 'uv'
 
 $CFLAGS << ' -O0 -ggdb -Wall'
+$LDFLAGS << ' -Wl,--no-as-needed' # stupid defaults are stupid
 
 create_makefile 'rbhelium'
--- test.rb
+++ test.rb
@@ -3,13 +3,17 @@ require 'base64'
 
 token = Base64.decode64("C8Slmiwm6dreZrUhy5YPiA==")
 
-conn = Helium::Connection.new do |datums|
-  puts "Got data #{datums}"
+conn = Helium::Connection.new do |mac, packet|
+  puts "Got data #{mac}, #{packet}"
 end
 
 puts "subscribing..."
 
 status = conn.subscribe(0x0000112233440001, token)
+
+puts "status: #{status}"
+
+status = conn.send(0x0000112233440001, token, "dhello, from ruby land")
 
 puts "status: #{status}"
 
