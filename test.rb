2a72c51f392cc62caacaa75b58ba945e60591790
Patrick Thomson <patrick@helium.co>
2014-09-29 10:10:42 -0400

    Create real minitest/unit unit test for rbhelium.

--- test.rb
+++ test.rb
@@ -1,16 +1,23 @@
 require_relative 'rbhelium'
 require 'base64'
+require 'minitest/unit'
+require 'minitest/autorun'
 
-token = Base64.decode64("C8Slmiwm6dreZrUhy5YPiA==")
+TEST_MAC = 0x0000112233440001
+TOKEN = Base64.decode64("C8Slmiwm6dreZrUhy5YPiA==")
 
-conn = Helium::Connection.new do |mac, datums|
-  puts "Got data #{datums} from mac #{mac}"
+class TestHeliumSubscriptions < MiniTest::Unit::TestCase
+  def setup()
+    @succeeded = false
+    @connection = Helium::Connection.new do |mac, datums|
+      @succeeded = true
+    end
+  end
+
+  def test_add_subscription
+    status = @connection.subscribe(TEST_MAC, TOKEN)
+    assert_equal(status, 0)
+    sleep(5)
+    assert(@succeeded)
+  end
 end
-
-puts "subscribing..."
-
-status = conn.subscribe(0x0000112233440001, token)
-
-puts "status: #{status}"
-
-sleep 15
