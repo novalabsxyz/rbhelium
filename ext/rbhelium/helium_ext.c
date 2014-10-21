c3b0c51c512f9233d02522663537ddeac346560e
Patrick Thomson <patrick@helium.co>
2014-10-21 11:11:55 -0400

    add gem skeleton

--- ext/rbhelium/helium_ext.c
+++ ext/rbhelium/helium_ext.c
@@ -59,7 +59,7 @@ void helium_rb_callback(const helium_connection_t *conn, uint64_t sender_mac, ch
 
 static VALUE helium_rb_allocate(VALUE klass)
 {
-  return Data_Wrap_Struct(klass, NULL, helium_free, helium_alloc(NULL));
+  return Data_Wrap_Struct(klass, NULL, helium_free, helium_alloc());
 }
 
 /*
